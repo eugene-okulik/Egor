9589fbcd7894c09180a489a775cc9655ab5bf8b5
Egor Kovalenko <egorkovalenko1988@gmail.com>
2024-01-11 23:01:10 +0400

    updated file1 from lesson_4

--- homework/Egor_Kovalenko/lesson_4/File1.py
+++ homework/Egor_Kovalenko/lesson_4/File1.py
@@ -6,7 +6,7 @@ my_dict['set'] = {1, 2, 3, 4, 5}
 print(my_dict['tuple'][-1])
 my_dict['list'].append(56)
 my_dict['list'].pop(1)
-my_dict['i am a tuple'] = 'i am not a value'
+my_dict[('i am a tuple',)] = 'i am not a value'
 my_dict['dict'].pop('one')
 my_dict['set'].add('test')
 my_dict['set'].remove(2)
