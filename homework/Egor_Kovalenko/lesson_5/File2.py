137f4dce24d108055ab1f9b828dbabdf0d32fd3d
Egor Kovalenko <egorkovalenko1988@gmail.com>
2024-01-15 14:58:41 +0400

    files from homework_5

---
+++ homework/Egor_Kovalenko/lesson_5/File2.py
@@ -0,0 +1,13 @@
+text = 'результат операции: 42'
+text_index = text.index('42')
+print(text_index + 10)
+
+text_1 = 'результат операции: 514'
+text_slice = int(text_1[20:])
+result_1 = text_slice + 10
+print(result_1)
+
+text_2 = 'результат работы программы: 9'
+text_index1 = text_2.index('9')
+result_2 = text_index1 + 10
+print(result_2)
---
+++ homework/Egor_Kovalenko/lesson_5/File3.py
@@ -0,0 +1,3 @@
+students = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+print(', '.join(students), 'study these subjects:', ', '.join(subjects))
