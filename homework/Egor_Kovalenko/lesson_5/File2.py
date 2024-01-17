6c487d60bc8bea0346b58a7ba81b88e8519e8da1
Egor Kovalenko <egorkovalenko1988@gmail.com>
2024-01-17 21:20:15 +0400

    Added whitespace in File2 in homework_5

--- homework/Egor_Kovalenko/lesson_5/File2.py
+++ homework/Egor_Kovalenko/lesson_5/File2.py
@@ -1,14 +1,14 @@
 text = 'результат операции: 42'
 text_index = text.index(':')
-text_slice = int(text[text_index+2:]) + 10
+text_slice = int(text[text_index + 2:]) + 10
 print(text_slice)
 
 text_2 = 'результат работы программы: 9'
 text_index1 = text_2.index(':')
-result_2 = int(text_2[text_index1+1:]) + 10
+result_2 = int(text_2[text_index1 + 1:]) + 10
 print(result_2)
 
 str_1 = 'результат операции: 514'
 my_slice = str_1.index(':')
-aa = int(str_1[my_slice+2:]) + 10
+aa = int(str_1[my_slice + 2:]) + 10
 print(aa)
