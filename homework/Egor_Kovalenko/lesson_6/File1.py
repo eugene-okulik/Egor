7dc0b038256cdb8cc0efd0d82ce14b980476805f
Egor Kovalenko <egorkovalenko1988@gmail.com>
2024-01-27 15:06:44 +0400

    updated file1 in lesson_6

--- homework/Egor_Kovalenko/lesson_6/File1.py
+++ homework/Egor_Kovalenko/lesson_6/File1.py
@@ -1,16 +1,17 @@
 text = \
-    'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+    'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. \
+    Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
 words = text.split()
 new_text = ''
 for word in words:
     punctuation = ''
-    if word[-1] in [',', '.']:  
-        punctuation = word[-1]  
-        word = word[:-1]  
+    if word[-1] in [',', '.']:
+        punctuation = word[-1]
+        word = word[:-1]
 
-    if len(word) >= 2:  
-        word += 'ing'  
+    if len(word) >= 2:
+        word += 'ing'
 
-    word += punctuation  
+    word += punctuation
     new_text += word + ' '
 print(new_text)
